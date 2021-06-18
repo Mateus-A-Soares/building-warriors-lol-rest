7dcf295bcca3b97a1d929c5dec2d8e701c209c7f
Mateus-A-Soares <soares.almeida.mateus@gmail.com>
2021-06-18 17:19:45 -0300

    Refatoração da tratativa de ConstraintExceptions para um handler customizado

--- src/main/kotlin/br/com/iupp/buildingwarriors/controller/champion/ChampionController.kt
+++ src/main/kotlin/br/com/iupp/buildingwarriors/controller/champion/ChampionController.kt
@@ -2,14 +2,11 @@ package br.com.iupp.buildingwarriors.controller.champion
 
 import br.com.iupp.buildingwarriors.controller.champion.request.ChampionRequest
 import br.com.iupp.buildingwarriors.controller.champion.response.ChampionResponse
-import br.com.iupp.buildingwarriors.controller.handler.response.DefaultErrorResponse
 import br.com.iupp.buildingwarriors.service.ChampionService
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
-import io.micronaut.http.HttpStatus
 import io.micronaut.http.annotation.*
 import io.micronaut.validation.Validated
-import javax.validation.ConstraintViolationException
 import javax.validation.Valid
 import javax.validation.constraints.Positive
 
@@ -58,23 +55,5 @@ class ChampionController(
     fun deleteChampion(@PathVariable @Positive(message = "Deve ser um numero positivo") id: Long): HttpResponse<Unit> {
         service.deleteChampion(id)
         return HttpResponse.noContent()
-    }
-
-    @Error(exception = ConstraintViolationException::class)
-    fun constraintExceptionHandler(
-        request: HttpRequest<*>,
-        exception: ConstraintViolationException
-    ): HttpResponse<DefaultErrorResponse> {
-        val messages = exception.constraintViolations.map {
-            "${it.propertyPath.last()}: ${it.message}"
-        }
-        return HttpResponse.unprocessableEntity<DefaultErrorResponse>().body(
-            DefaultErrorResponse(
-                statusCode = HttpStatus.UNPROCESSABLE_ENTITY.code,
-                messages = messages,
-                path = request.path,
-                error = HttpStatus.UNPROCESSABLE_ENTITY.reason
-            )
-        )
     }
 }
